460251e215452d478a4278e302e422fe896a1ccf
lauti910 <lauti910@gmail.com>
2017-03-23 15:51:09 -0300

    Creado el ejemplo de singleton

---
+++ Workspace/BootCamp2017/src/Clase100P/SingletonExample.java
@@ -0,0 +1,18 @@
+package Clase100P;
+
+public class SingletonExample {
+	
+	private static SingletonExample instance = null;
+	private SingletonExample() {
+	}
+	static public SingletonExample getInstance(){
+		if(instance == null){
+			instance = new SingletonExample();
+		}
+		return instance;
+	}
+	
+	public void getConnection(){
+		//Should return a connection with a database
+	}
+}
