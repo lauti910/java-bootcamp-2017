97522eb74805aa3a687d0649cf7f4c40ba34cd4e
lauti910 <lauti910@gmail.com>
2017-03-23 21:08:37 -0300

    Creado el ejemplo de proxy

---
+++ Workspace/BootCamp2017/src/Clase1/Singleton/Connection.java
@@ -0,0 +1,14 @@
+package Clase1.Singleton;
+
+public class Connection {
+	//represents a connection with the database
+	
+	public void get(String something) {
+		//returns something from the database
+	}
+
+	public void close() {
+		//close the connection
+		
+	}
+}
