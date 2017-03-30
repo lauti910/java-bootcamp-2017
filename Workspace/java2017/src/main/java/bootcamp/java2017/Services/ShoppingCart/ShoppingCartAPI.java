1bb10fa1668dcc2fccf5a40dcdd2e945d8702cb0
lauti910 <lauti910@gmail.com>
2017-03-30 16:03:12 -0300

    added a method, and commented what should each method do

--- Workspace/java2017/src/main/java/bootcamp/java2017/Services/ShoppingCart/ShoppingCartAPI.java
+++ Workspace/java2017/src/main/java/bootcamp/java2017/Services/ShoppingCart/ShoppingCartAPI.java
@@ -1,18 +1,30 @@
 package bootcamp.java2017.Services.ShoppingCart;
 
 public interface ShoppingCartAPI {
+	
+	//Adds an item to the cart
 	public void addItem(Item item);
 	
-	// throws an exeption when you try to remove an item that isnt there
+	//Removes an item from the cart
+	//Throws an exception when you try to remove an item that isnt there
 	public void removeItem(Item item) throws ItemNotFoundExeption;
 	
+	//Returns the price of all the items in the cart, without discounts
 	public Double getTotalPrice();
 	
+	//Returns the price of all the items in the cart, including discounts
+	public Double getActualPrice();
+	
+	//Sets the form of payment to pay afterwards
+	//Should be a default form in case no-one is selected
 	public void setFormOfPayment(FormOfPayment payment);
 	
+	//Returns the list of items added to the cart.
+	//The ItemList shouldn't be modifiable
 	public ItemList getItems();
 	
-	//throws an exeption when there isnt enought money to pay for the items you want
+	//Pay for the items through a form of payment
+	//Throws an exception when there isn't enough money to pay for the items you want
 	public void pay() throws NotEnoughMoneyExeption;
 	
 	
