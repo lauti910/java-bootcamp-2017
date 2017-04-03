ac8670dde5fe7e006226995188cb916d670bc934
lauti910 <lauti910@gmail.com>
2017-04-02 23:08:16 -0300

    An offer can include other offers

--- Workspace/java2017/src/main/java/bootcamp/java2017/Services/ShoppingCart/Offers/Offer.java
+++ Workspace/java2017/src/main/java/bootcamp/java2017/Services/ShoppingCart/Offers/Offer.java
@@ -1,19 +1,24 @@
 package bootcamp.java2017.Services.ShoppingCart.Offers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bootcamp.java2017.Services.ShoppingCart.Items.Item;
 import bootcamp.java2017.Services.ShoppingCart.Items.ItemList;
+import bootcamp.java2017.Services.ShoppingCart.Items.ItemListImpl;
 
 public class Offer {
 	
 	private String name;
 	private Double price;
 	private List<Item> items;
+	private List<Offer> offers;
 	
 	public Offer(String name, Double price){
 		this.name = name;
 		this.price = price;
+		this.items = new ArrayList<Item>();
+		this.offers = new ArrayList<Offer>();
 	}
 	public void addItem(Item item){
 		this.items.add(item);
@@ -22,11 +27,19 @@ public class Offer {
 		this.items.remove(item);
 	}
 	public Boolean canBeApplied(ItemList itemList){
-		return this.items.stream().allMatch(item -> itemList.contains(item));
+		Boolean result = false;
+		for(Offer of: this.offers){
+			result = result || of.canBeApplied(itemList);
+		}
+		return result || this.items.stream().allMatch(item -> itemList.contains(item));
 	}
 	
 	public void show(){
-		
+		System.out.println(this.name + "...... $" + this.price);
+		new ItemListImpl(this.items).show();
+		for(Offer of: this.offers){
+			of.show();
+		}
 	}
 	
 	public Double getPrice(){
