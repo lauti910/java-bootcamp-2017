787291b2835cb809005977e7491f8d3c4de61054
lauti910 <lauti910@gmail.com>
2017-04-03 00:24:09 -0300

    ItemList tested

---
+++ Workspace/java2017/src/test/java/bootcamp/java2017/Services/ItemListTest.java
@@ -0,0 +1,61 @@
+package bootcamp.java2017.Services;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import bootcamp.java2017.Services.ShoppingCart.Items.Item;
+import bootcamp.java2017.Services.ShoppingCart.Items.ItemList;
+import bootcamp.java2017.Services.ShoppingCart.Items.ItemListImpl;
+
+public class ItemListTest {
+	
+	ItemList itemList;
+	List<Item> arrayList;
+	Item item1;
+	Item item2;
+	Item item3;
+	@Before
+	public void setUp(){
+
+		this.item1 = Mockito.mock(Item.class);
+		this.item2 = Mockito.mock(Item.class);
+		this.item3 = Mockito.mock(Item.class);
+		this.arrayList = new ArrayList<Item>();
+		this.arrayList.add(item1);
+		this.arrayList.add(item2);
+		this.arrayList.add(item3);
+		this.itemList = new ItemListImpl(this.arrayList);
+		
+	}
+
+	@Test
+	public void test_AnEmptyListIsEmpty_AndAListWithItemsIsnt(){
+		ItemList empty = new ItemListImpl(new ArrayList<Item>());
+		
+		assertTrue(empty.isEmpty());
+		assertFalse(this.itemList.isEmpty());
+	}
+
+	@Test
+	public void test_theListContainsItems() {
+		assertTrue(this.itemList.contains(item1));
+		assertTrue(this.itemList.contains(item2));
+		assertTrue(this.itemList.contains(item3));
+	}
+	@Test
+	public void test_theListKnowsWichIsTheMostExpensive_AndTheCheapest_Item(){
+		Mockito.doReturn(1.0).when(item1).getPrice();
+		Mockito.doReturn(20.0).when(item2).getPrice();
+		Mockito.doReturn(300.0).when(item3).getPrice();
+		
+		assertEquals(this.itemList.getCheapestItem(), item1);
+		assertEquals(this.itemList.getMostExpensiveItem(), item3);
+	}
+
+}
