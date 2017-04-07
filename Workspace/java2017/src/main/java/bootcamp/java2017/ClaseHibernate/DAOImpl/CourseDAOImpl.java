17bfeca567d7760b4527e6092e80724067c2e917
lauti910 <lauti910@gmail.com>
2017-04-07 14:28:39 -0300

    Commented the implementation of courseDAO

--- Workspace/java2017/src/main/java/bootcamp/java2017/ClaseHibernate/DAOImpl/CourseDAOImpl.java
+++ Workspace/java2017/src/main/java/bootcamp/java2017/ClaseHibernate/DAOImpl/CourseDAOImpl.java
@@ -8,43 +8,83 @@ import org.hibernate.Transaction;
 import bootcamp.java2017.ClaseHibernate.DAO.CourseDAO;
 import bootcamp.java2017.ClaseHibernate.Model.Course;
 
-public class CourseDAOImpl implements CourseDAO{
+public class CourseDAOImpl implements CourseDAO {
 	private HibernateUtils utils;
+
+	/**
+	 * Should be done after you open a session, and you should close the session
+	 * when you finish
+	 * 
+	 * Persist the course in the database
+	 * @param course
+	 */
 	@Override
 	public void persist(Course course) {
 		this.utils.getCurrentSession().persist(course);
 	}
 
+	/**
+	 * Should be done after you open a session, and you should close the session
+	 * when you finish
+	 * 
+	 * @param id
+	 * @return Returns the Course with id == id
+	 */
 	@Override
 	public Course findById(Integer id) {
 		// TODO Auto-generated method stub
 		return this.utils.getCurrentSession().get(Course.class, id);
 	}
 
+	/**
+	 * Should be done after you open a session, and you should close the session
+	 * when you finish
+	 * deletes the course given
+	 * @param course
+	 */
 	@Override
 	public void delete(Course course) {
 		this.utils.getCurrentSession().delete(course);
-		
+
 	}
 
+	/**
+	 * Should be done after you open a session, and you should close the session
+	 * when you finish
+	 * 
+	 * Updates the course given
+	 * @param course
+	 */
 	@Override
 	public void update(Course course) {
 		this.utils.getCurrentSession().update(course);
-		
+
 	}
 
+	/**
+	 * Should be done after you open a session, and you should close the session
+	 * when you finish
+	 * 
+	 * @return Returns all the courses in the database
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Course> getAll() {
 		return (List<Course>) this.utils.getCurrentSession().createQuery("from Course").list();
 	}
 
+	/**
+	 * Should be done after you open a session, and you should close the session
+	 * when you finish
+	 * 
+	 * this delets all the courses in the databases. Use it carefully
+	 */
 	@Override
 	public void deleteAll() {
 		List<Course> all = this.getAll();
-		for(Course c: all){
+		for (Course c : all) {
 			this.delete(c);
 		}
 	}
-	
+
 }
